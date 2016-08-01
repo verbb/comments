d5305437080b3c8ff11033b81b48c9a35aebdbdb
thenicouk <thenicouk@users.noreply.github.com>
2016-08-01 11:43:55 +0100

    Stop HTML from being rendered in the admin section

--- comments/services/CommentsService.php
+++ comments/services/CommentsService.php
@@ -216,8 +216,8 @@ class CommentsService extends BaseApplicationComponent
             $html = '<div class="comment-block">';
             $html .= '<span class="status ' . $context['element']->status . '"></span>';
             $html .= '<a href="' . $context['element']->getCpEditUrl() . '">';
-            $html .= '<span class="username">' . $userName . '</span>';
-            $html .= '<small>' . $context['element']->getExcerpt(0, 100) . '</small></a>';
+            $html .= '<span class="username">' . htmlspecialchars($userName) . '</span>';
+            $html .= '<small>' . htmlspecialchars($context['element']->getExcerpt(0, 100)) . '</small></a>';
             $html .= '</div>';
 
             return $html;
