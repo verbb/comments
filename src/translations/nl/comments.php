8a7d366df1db773b470b3feb04482f37137f3704
Arjan <info@skoften.net>
2019-06-12 18:13:55 +0200

    Add missing translations

--- src/translations/nl/comments.php
+++ src/translations/nl/comments.php
@@ -20,4 +20,27 @@ return [
     'Your email' => 'Je email',
     'Add a comment...' => 'Plaats een reactie...',
     'Post comment' => 'Plaatsen'
+    
+    //  
+    // Email templates
+    //
+
+    'Your name' => 'Je naam',
+    'Your email' => 'Je email',
+    'Add a comment...' => 'Plaats een reactie...',
+    'Post comment' => 'Plaatsen',
+
+	//
+	// validation
+	//
+
+    'Form validation failed. Marked as spam.' => 'Je reactie is gedetecteerd als spam en dus niet verstuurd.',
+    'Comment blocked due to security policy.' => 'Reactie geblokkeerd i.v.m. veiligheidsredenen.',
+    'Comment must be shorter than 1000 characters.' => 'Reactie mag niet langer zijn dan 1000 karakters',
+    'Must be logged in to comment.' => 'Inloggen verplicht.',
+    'Name is required.' => 'Naam is vereist.',
+    'Email is required.' => 'Email is vereist.',
+    'Unable to modify another users comment.' => 'Je kan andermans reactie niet bewerken.',
+    'Comment must not be blank.' => 'Reactie mag niet leeg zijn.',
+    'Comments are disabled for this element.' => 'Reacties zijn uitgeschakeld.'
 ];
