8105913c2e753be5ce68a22a3f07d4c12b12223a
Arjan <info@skoften.net>
2019-06-12 18:12:23 +0200

    add missing translations

--- src/translations/en/comments.php
+++ src/translations/en/comments.php
@@ -15,4 +15,27 @@ return [
     'comments_reply_notification_body' => "Hey {{user.friendlyName}},\n\n" .
         "A new reply to your comment on the post \"{{ element.title }}\" has been made.\n\n" .
         "{{element.url}}#comment-{{comment.id}}.",
+    
+    //  
+    // Email templates
+    //
+
+    'Your name' => 'Your name',
+    'Your email' => 'Email',
+    'Add a comment...' => 'Write a comment',
+    'Post comment' => 'Submit',
+    
+	//
+	// validation
+	//
+	
+	'Form validation failed. Marked as spam.' => 'Oops, comment validation failed. Marked as spam.',
+	'Comment blocked due to security policy.' => 'Comment blocked due to security policy.',
+	'Comment must be shorter than 1000 characters.' => 'Comment must be shorter than 1000 characters.',
+	'Must be logged in to comment.' => 'Must be logged in to comment.',
+	'Name is required.' => 'Name is required.',
+	'Email is required.' => 'Email is required.',
+	'Unable to modify another users comment.' => 'Unable to modify another users comment.',
+	'Comment must not be blank.' => 'Comment must not be blank.',
+	'Comments are disabled for this element.' => 'Comments are disabled for this element.' 
 ];
