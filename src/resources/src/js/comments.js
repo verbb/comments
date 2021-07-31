408291ea3a001d51cf51243c879ab92466a12e60
Josh Crawford <joshua@sgroup.com.au>
2021-07-31 18:12:05 +1000

    Minor styling

--- src/resources/src/js/comments.js
+++ src/resources/src/js/comments.js
@@ -211,17 +211,17 @@ Comments.Base = Base.extend({
         return (Comments.translations.hasOwnProperty(key)) ? Comments.translations[key] : '';
     },
 	
-	makeUniqueID(prefix = 'ID') {
+	makeUniqueID: function(prefix = 'ID') {
 		var d = new Date().getTime();
 
-		if( window.performance && typeof window.performance.now === "function" ) {
+		if (window.performance && typeof window.performance.now === 'function') {
 			d += performance.now();
 		}
 
 		var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-			var r = (d + Math.random()*16)%16 | 0;
+			var r = (d + Math.random() * 16) % 16 | 0;
 			d = Math.floor(d/16);
-			return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+			return (c == 'x' ? r : (r&0x3|0x8)).toString(16);
 		});
 
 		return prefix + '_' + uuid;
