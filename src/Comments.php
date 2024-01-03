79a6dcce2b16ee8e2ab6cc6616bad7326c4083f3
Josh Crawford <joshua@sgroup.com.au>
2024-01-04 10:09:03 +1100

    Removed deprecation handling for `allowAnonymousFlagging`. Use `allowGuestFlagging` instead

--- src/Comments.php
+++ src/Comments.php
@@ -399,7 +399,6 @@ class Comments extends Plugin
 
         // Check for renamed settings
         $renamedSettings = [
-            'allowAnonymousFlagging' => 'allowGuestFlagging',
             'securityBlacklist' => 'securitySpamlist',
         ];
 
