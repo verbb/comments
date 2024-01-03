a0b81c604747ecedabed98e20f0882f74e0d577f
Josh Crawford <joshua@sgroup.com.au>
2024-01-04 10:08:56 +1100

    Removed deprecation handling for `allowAnonymousVoting`. Use `allowGuestVoting` instead

--- src/Comments.php
+++ src/Comments.php
@@ -399,7 +399,6 @@ class Comments extends Plugin
 
         // Check for renamed settings
         $renamedSettings = [
-            'allowAnonymousVoting' => 'allowGuestVoting',
             'allowAnonymousFlagging' => 'allowGuestFlagging',
             'securityBlacklist' => 'securitySpamlist',
         ];
