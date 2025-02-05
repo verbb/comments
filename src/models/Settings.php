9ed0cf9d6e151362e875af9973da8a7cb19405e1
Josh Crawford <joshua@sgroup.com.au>
2025-02-05 14:57:17 +1100

    Add support for `notificationAdmins` to be a Closure

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -16,6 +16,8 @@ use craft\helpers\ArrayHelper;
 use craft\helpers\Db;
 use craft\helpers\Json;
 
+use Closure;
+
 class Settings extends Model
 {
     // Properties
@@ -87,7 +89,7 @@ class Settings extends Model
     public bool $notificationModeratorEnabled = false;
     public bool $notificationModeratorEditEnabled = false;
     public bool $notificationModeratorApprovedEnabled = false;
-    public array $notificationAdmins = [];
+    public array|Closure $notificationAdmins = [];
     public bool $notificationAdminEnabled = false;
     public bool $notificationFlaggedEnabled = false;
     public bool $useQueueForNotifications = false;
@@ -191,9 +193,19 @@ class Settings extends Model
         return null;
     }
 
+    public function getNotificationAdmins(): array
+    {
+        // Support closures
+        if ($this->notificationAdmins instanceof Closure) {
+            return ($this->notificationAdmins)();
+        }
+
+        return $this->notificationAdmins;
+    }
+
     public function getEnabledNotificationAdmins(): array
     {
-        $notificationAdmins = $this->notificationAdmins ?: [];
+        $notificationAdmins = $this->getNotificationAdmins() ?: [];
 
         return ArrayHelper::where($notificationAdmins, 'enabled');
     }
