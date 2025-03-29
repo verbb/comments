7681883668ad072a3a0fa941c25c340e3812c92f
Josh Crawford <joshua@sgroup.com.au>
2025-03-29 12:17:15 +1100

    Fix an error when eager loading owner and user for comments

--- src/elements/Comment.php
+++ src/elements/Comment.php
@@ -1043,9 +1043,9 @@ class Comment extends Element
     public function setEagerLoadedElements(string $handle, array $elements, EagerLoadPlan $plan): void
     {
         if ($handle === 'user') {
-            $this->_user = $elements[0] ?? false;
+            $this->_user = $elements[0] ?? null;
         } else if ($handle === 'owner') {
-            $this->_owner = $elements[0] ?? false;
+            $this->_owner = $elements[0] ?? null;
         } else {
             parent::setEagerLoadedElements($handle, $elements, $plan);
         }
